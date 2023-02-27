4455ae623d0ca9ae032d9f295b2837cf17aa1754
faizan <sfh8881212@gmail.com>
2023-02-27 10:37:37 -0800

    d

--- insert-data.php
+++ insert-data.php
@@ -9,14 +9,16 @@
 
     <link rel="stylesheet" href="css/bootstrap.css">
     <style>
-        #error-message {
+        #error-message
+        {
             display: none;
-
+            
         }
-
-        #success-message {
+        #success-message
+        {
             display: none;
         }
+        
     </style>
 </head>
 
@@ -24,19 +26,18 @@
     <div class="container">
         <h1>Create New Record using AJAX PHP</h1>
         <table class="table">
-            <form id="addform">
-                <tr>
-                    <td>Name</td>
-                    <td><input type="text" required id="name" class="form-control"></td>
-                </tr>
-                <tr>
+           <form id="addform">
+           <tr>
+                <td>Name</td>
+                <td><input type="text" required id="name" class="form-control"></td>
+            </tr>
+            <tr>
 
-                    <td><input type="submit" id="savebtn" name="btn" class="btn btn-danger"></td>
-                </tr>
-            </form>
-
+                <td><input type="submit" id="savebtn" name="btn" class="btn btn-danger"></td>
+            </tr>
+           </form>
+           
             <div class="alert alert-danger" id="error-message" role="alert"></div>
-            <div class="alert alert-success" id="success-message" role="alert"></div>
 
             <tr>
                 <td colspan="2" id="table-data"></td>
@@ -64,8 +65,8 @@
                     var fname = $("#name").val();
 
                     if (fname == "") {
-                        $("#success-message").slideUp();
                         $("#error-message").html("All Feild are Required").slideDown();
+                    
                     } else {
                         $.ajax({
                             url: "ajax-insert.php",
@@ -77,16 +78,14 @@
                                 if (data == 1) {
                                     loadtable();
                                     $("#addform").trigger("reset");
-                                    $("#error-message").slideUp();
-                                    
-                                    $("#success-message").html("Data Save").slideDown();
-                                    
+                                    $("#error-message").html("Data Save").slideDown();
                                 } else {
-                                    $("#error-message").html("Not Save");
+                                    $("#error-message").html("All Feild are Required");
                                 }
                             }
                         });
                     }
+
                 });
             });
         </script>
